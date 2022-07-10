c893f45178fcedbd71848c3ec1a593e09ac4feb5
prince <vasukashyap08@gmail.com>
2022-07-10 20:55:27 +0530

    initial-commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: true,
+    loop: true,
+    breakpoints: {
+        100: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        568: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+      },
+  });
