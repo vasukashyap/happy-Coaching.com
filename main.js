f11c593bed2e15a6db279a37909f2b2121aa55e6
prince <vasukashyap08@gmail.com>
2022-07-12 22:42:05 +0530

    tuseday-commit

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoplay: true,
-    loop: true,
-    breakpoints: {
-        100: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        568: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-      },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: true,
+  loop: true,
+  breakpoints: {
+    100: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    568: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  },
+});
+
+const faq = document.querySelectorAll(".faq");
+
+faq.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  })
+
+});
